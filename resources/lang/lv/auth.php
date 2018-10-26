ce83c3a3aa2d097c998b9b5bf74eb33ce5a4ff5d
Edgars Vanags <ev@piemeram.lv>
2018-10-26 10:37:37 +0300

    Removed unnecessary code and fixed blade identation Added lv language

---
+++ resources/lang/lv/auth.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Šie akreditācijas dati neatbilst mūsu reģistriem',
+    'throttle' => 'Pārāk daudz ienākšanas mēģinājumi. Lūdzu mēģini vēlreiz pēc :seconds sekundēm.',
+
+    'passwords' => [
+        'reset' => [
+            'title' => 'Atiestatīt paroli',
+            'email' => 'E-pasts',
+            'password' => 'Parole',
+            'passwordagain' => 'Parole vēlreiz',
+        ],
+    ],
+    'verify' => [
+        'title' => 'Aplieciniet savu e-pasta adresi',
+        'beforeproceeding' => 'Pirms turpiniet, lūdzu pārbaudiet e-pastu vai jums jau nav apliecinājuma saite',
+        'resend' => 'Sūtīt apliecinājuma e-pastu',
+        'emailsent' => 'Jauna apliecinājuma saite nosūtīta uz jūsu e-pasta adresi',
+    ],
+];
---
+++ resources/lang/lv/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Parolei jāsastāv no vismaz 6 simboliem un jāsakrīt ar atkārtoto paroli.',
+    'reset' => 'Parole veiksmīgi atiestatīta!',
+    'sent' => 'Uz tavu e-pastu tika nosūtīts atiestatīšanas links!',
+    'token' => 'Šīs paroles atiestatīšanas tokens ir nepareizs.',
+    'user' => "Nav iespējams atrast lietotāju ar šādu e-pasta adresi.",
+];
