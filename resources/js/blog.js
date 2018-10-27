764b63860e9710421b9236ef0394e4fcc4e56e21
Edgars Vanags <ev@piemeram.lv>
2018-10-27 14:00:25 +0300

    Removed Vue from window

--- resources/js/blog.js
+++ resources/js/blog.js
@@ -3,14 +3,12 @@ import VueRouter from 'vue-router'
 import routes from './routes'
 import Blog from './components/blog.vue'
 
-window.Vue = Vue
-
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes
 })
 
-const blog = new window.Vue({
+const blog = new Vue({
   el: '#blog',
   router,
   components: {
