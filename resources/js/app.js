563b76e09ecc2c2b1cae47a30fdca9e6c42acc09
Edgars Vanags <ev@piemeram.lv>
2018-10-27 12:51:02 +0300

    Added Vue FIxed js file identation

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+window.Vue = require('vue')
+
+const blog = new Vue({
+  el: '#blog',
+  data: () => ({
+    greeting: 'Welcome to my blog'
+  })
+})
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,9 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
   </head>
   <body>
-    Welcome to my blog
+    <div id="blog">
+      @{{ greeting }}
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
