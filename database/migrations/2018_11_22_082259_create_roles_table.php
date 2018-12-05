94ecc05a2f6e67f991bd63e10a08a630a79a1599
Edgars Vanags <ev@piemeram.lv>
2018-12-05 13:52:30 +0200

    Fixed migrations Fixed translations

--- database/migrations/2018_11_22_082259_create_roles_table.php
+++ database/migrations/2018_11_22_082259_create_roles_table.php
@@ -24,6 +24,43 @@ class CreateRolesTable extends Migration
             $table->timestamps();
         });
 
+        $now = now();
+        $roles = [
+            [
+                'name_en' => 'Developer',
+                'name_lv' => 'Izstrādātājs',
+                'description_en' => 'Blog maintainer',
+                'description_lv' => 'Emuāra uzturētājs',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'name_en' => 'User',
+                'name_lv' => 'Lietotājs',
+                'description_en' => 'Default role',
+                'description_lv' => 'Noklusējuma loma',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'name_en' => 'Administrator',
+                'name_lv' => 'Administrators',
+                'description_en' => 'Full access to user information',
+                'description_lv' => 'Pilna pieeja lietotāju informācijai',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'name_en' => 'Writer',
+                'name_lv' => 'Rakstnieks',
+                'description_en' => 'Writes posts',
+                'description_lv' => 'Raksta ziņas',
+                'created_at' => $now,
+                'updated_at' => $now,
+            ]
+        ];
+        DB::table('roles')->insert($roles);
+
         Schema::table('users', function (Blueprint $table) {
             $table->integer('role_id')->nullable();
 
@@ -32,6 +69,10 @@ class CreateRolesTable extends Migration
                 ->on('roles')
                 ->onDelete('set null');
         });
+
+        DB::table('users')
+            ->whereId(1)
+            ->update(['role_id' => 1]);
     }
 
     /**
